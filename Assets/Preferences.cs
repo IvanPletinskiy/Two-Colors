5edcd340aeb77798ab47d1790ca8f610359cf006
Ivan School <ivanhanden@gmail.com>
2018-03-22 08:45:13 +0300

    Added two methods

---
+++ Assets/Preferences.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Preferences {
+	public static string DIALOG_WELCOME = "dialog_welcome";
+	public static string DIALOG_RATE = "dialog_rate";
+
+	public static int getBool(string key) {
+		return PlayerPrefs.GetInt (key);
+	}
+
+	public static void setBool(string key, bool value) {
+		PlayerPrefs.SetInt (key, (int) value);
+	}
+
+	public static int getInt(string key) {
+		
+	}
+
+}
