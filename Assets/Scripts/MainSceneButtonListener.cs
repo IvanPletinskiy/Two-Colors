77f2804a4f3fa13de44ac23557ce0ff35d0b3f0a
Handen <ivanhanden@gmail.com>
2018-04-03 00:07:47 +0300

    Nice La!

---
+++ Assets/Scripts/MainSceneButtonListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneButtonListener : MonoBehaviour {
+
+    public string action;
+
+	void OnMouseDown()
+    {
+
+    }
+
+    void OnMouseUp()
+    {
+
+    }
+
+    void OnMouseUpAsButton()
+    {
+        switch (action)
+        {
+            case "Play":
+                Application.LoadLevel("Play");
+                break;
+        }
+    }
+}
