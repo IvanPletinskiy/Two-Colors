89a06d02e8ea371b481770236bfb607d0e7c6846
Handen <ivanhanden@gmail.com>
2018-04-04 00:06:24 +0300

    TilesScript

--- Assets/Scripts/TilesScript.cs
+++ Assets/Scripts/TilesScript.cs
@@ -1,16 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TilesScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Text scoreText, levelText;
+   
+
+    public GameObject[] tiles = new GameObject[4];
+    public Vector4[] colors = new Vector4[4];
+
+    int level = 0;
+    int score = 0;
+
+    public bool next, lose;
+
+    // Use this for initialization
+    void Start () {
+        next = false;
+        lose = false;
+        //tiles[0] = gameObject.transform.GetChild(0);
+        updateLevel();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (lose)
+            endGame();
+        if (next && !lose)
+            updateLevel();
+    }
+
+    private void updateLevel()
+    {
+        level++;
+
+        scoreText.text = score.ToString();
+        levelText.text = "Level " + level.ToString();
+
+        updateTiles();
+    }
+
+    private void updateTiles()
+    {
+        generateColors();
+
+        for(int i = 0; i < 4; i ++)
+        {
+        //    tiles[i].GetComponent<Renderer>.material.color = colors[i];
+        }
+    }
+
+    private void generateColors()
+    {
+        for(int i = 0; i < 4; i ++)
+        {
+            Color color = new Vector4(Random.Range(0.1f, 1f), Random.Range(0.1f, 1f), Random.Range(0.1f, 1f), 1);
+
+        //    tiles[i].GetComponent<Renderer>.material.color = aColor;
+
+           // GetComponent<Renderer>().material.color = aColor;
+        }
+    }
+
+    private void checkResult()
+    {
+
+    }
+
+    private void endGame()
+    {
+
+    }
+
+
 }
