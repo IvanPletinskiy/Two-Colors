f3521a15e985791ea699803ec81b84d4ed7f3552
Handen <ivanhanden@gmail.com>
2018-04-23 18:18:26 +0300

    Этот коммит не работает как надо

--- Assets/Scripts/DialogRate.cs
+++ Assets/Scripts/DialogRate.cs
@@ -2,43 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogRate : MonoBehaviour {
+public class DialogRate : MonoBehaviour
+{
 
-	public GameObject dialogRate;
+    public static GameObject dialogRate;
 
-	public Camera mainCamera;
+    public Camera mainCamera;
 
-	public static bool isDialogRate=false;
+    //	public static bool isDialogRate=false;
+    public static int ATTEMPTSFORDIALOG = 10;
 
-	void Update () {
-		if (isDialogRate && PlayerPrefs.GetInt("IsRate") != 1 && RespawnScript.isHeard == false)
-			DialogRateShow ();
-		
-		if (Input.GetKeyUp (KeyCode.Mouse0)) {
-			Ray ray = mainCamera.ScreenPointToRay (new Vector2 (Input.mousePosition.x,Input.mousePosition.y));
-			RaycastHit hit;
-			if (Physics.Raycast(ray,out hit)) {
-				if (hit.collider.tag == "Button") {
-					dialogRate.SetActive (false);
-					isDialogRate = false;
-					Time.timeScale = 1;
-				}
-				if (hit.collider.name == "Yes") {
-					Application.OpenURL ("https://play.google.com/store/apps/details?id=com.handen.twocolors");
-				}
-				if (hit.collider.name == "No, thanks"){
-					PlayerPrefs.SetInt ("IsRate", 1);
-					dialogRate.SetActive (false);
-					isDialogRate = false;
-					Time.timeScale = 1;
-				}
-			}
+    void Update()
+    {
+        //		if (isDialogRate && PlayerPrefs.GetInt("IsRate") != 1 && RespawnScript.isHeard == false)
+        //			DialogRateShow ();
 
-		}
-	}
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Button")
+                {
+                    dialogRate.SetActive(false);
+                    Preferences.resetAttempts();
+                    Time.timeScale = 1;
+                }
+                if (hit.collider.name == "Yes")
+                {
+                    Application.OpenURL("https://play.google.com/store/apps/details?id=com.handen.twocolors");
+                    dialogRate.SetActive(false);
+                    Preferences.setRateShown(true);
+                }
+                if (hit.collider.name == "No, thanks")
+                {
+                    Preferences.setRateShown(true);
+                    dialogRate.SetActive(false);
+                    //					isDialogRate = false;
+                    Time.timeScale = 1;
+                }
+            }
+        }
+    }
 
-	void DialogRateShow(){
-		dialogRate.SetActive (true);
-		Time.timeScale = 0;
-	}
+    public static void DialogRateShow()
+    {
+        dialogRate.SetActive(true);
+        Time.timeScale = 0;
+    }
 }
